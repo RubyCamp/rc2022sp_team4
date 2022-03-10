e6484f258517165a58be627f71f3a7d6b4c36d08
maruya0000 <marukou0731@icloud.com>
2022-03-10 15:27:54 +0900

    完成品

--- directors/ending_director.rb
+++ directors/ending_director.rb
@@ -6,56 +6,80 @@ module Directors
     # 初期化
     def initialize(screen_width:, screen_height:, renderer:)
       super
-
-      # テキスト表示用パネルを生成し、カメラから程よい距離に配置する
       
-      create_ending_object
+
+      # ゲーム開始時刻の設定
+      
+
+      @create_ending_object= create_ending_object
+      
+      
     end
 
-    
-      # RubyCampの8文字を、1文字1アニメーションパネルとして作成し、表示開始タイミングを微妙にずらす
-      
-    
     def create_ending_object()
-      @description = AnimatedPanel.new(width: 1, height: 0.25, start_frame: 15, map: TextureFactory.create_ending_description)
-      @description.mesh.position.z = -0.5
-      @description.mesh.position.y = 0.2
-      self.scene.add(@description.mesh)
+      flag=true
+      lambda do 
       
-      start_x = -0.1
+        return if flag==false 
+       
+        flag=false
+        @description = AnimatedPanel.new(width: 1, height: 0.25, start_frame: 15, map: TextureFactory.create_ending_description)
+        @description.mesh.position.z = -0.5
+        @description.mesh.position.y = 0.2
+        self.scene.add(@description.mesh)
 
-      %w(9 9 9).each_with_index do |char, idx|
-        create_ending_score(char, start_x + (idx * 0.1), idx * 2)
+        @description2 = AnimatedPanel.new(width: 0.25, height: 0.125, map: TextureFactory.create_ending_description_score)
+        @description2.mesh.position.z = -0.5
+        self.scene.add(@description2.mesh)
+
+        @description3 = AnimatedPanel.new(map: TextureFactory.create_ending_second)
+        @description3.mesh.position.z = -0.5
+        @description3.mesh.position.x = 0.2
+        @description3.mesh.position.y = -0.2
+        self.scene.add(@description3.mesh)
+
+        start_x = -0.1
+  
+        p "%03d" %  $exec_time.to_i
+     
+        ("%03d" %  $exec_time.to_i).chars.each_with_index do |char, idx|
+          create_ending_score(char, start_x + (idx * 0.1), idx * 2)
+        end
       end
     end
-    
 
     def create_ending_score(char, x_pos, delay_frames)
       panel = AnimatedPanel.new(start_frame: 30, map: TextureFactory.create_score(char))
       panel.mesh.position.x = x_pos
       panel.mesh.position.z = -0.5
+      panel.mesh.position.y = -0.15
       self.scene.add(panel.mesh)
       @panels ||= []
       @panels << panel
     end
 
-    
-    
     # 1フレーム分の進行処理
     def play
       # テキスト表示用パネルを1フレーム分アニメーションさせる
-      @description.play
+      
 
+      @create_ending_object.call
+      
+      @description.play
+      @description2.play
+      @description3.play
       @panels.each(&:play)
     end
 
     # キー押下（単発）時のハンドリング
     def on_key_pressed(glfw_key:)
       case glfw_key
-        # ESCキー押下で終了する
-        when GLFW_KEY_ESCAPE
-          puts 'クリア!!'
-          transition_to_next_director # self.next_directorがセットされていないのでメインループが終わる
+      # ESCキー押下で終了する
+      when GLFW_KEY_ESCAPE
+        
+        puts 'クリア!!'
+        transition_to_next_director # self.next_directorがセットされていないのでメインループが終わる
+      
       end
     end
   end
